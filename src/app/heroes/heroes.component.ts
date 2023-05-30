4618324a4d8f234210ed8832257c5c499b69d4f3
kushal-7 <chhetrikushal14@gmail.com>
2023-05-30 23:12:49 +0600

    updated feature_1

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,19 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heros';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes = HEROES;
   selectedHero?: Hero;
+  heroForm: FormGroup = new FormGroup({});
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.heroForm = this.formBuilder.group({
+      name: ['', Validators.required]
+    });
   }
 
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+    this.heroForm.patchValue({
+      name: this.selectedHero.name
+    });
+  }
+
+  onSubmit() {
+    if (this.heroForm.valid && this.selectedHero) {
+      return this.selectedHero.name = this.heroForm.value.name;
+    }
+  }
   
 }
+
 
