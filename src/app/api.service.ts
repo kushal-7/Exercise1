e06da2ba4d1cdfe062cf1b1c8f5944dcdf66e206
kushal-7 <chhetrikushal14@gmail.com>
2023-06-20 21:31:45 +0600

    Exercise_2

---
+++ src/app/api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  // need http from angular:
+  // get  method
+  constructor() { }
+}
